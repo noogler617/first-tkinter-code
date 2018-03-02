2ed0336290e3058bd3882d7b358b7a696ec614e0
Ryan <themolenator@gmail.com>
2018-03-02 16:59:22 -0500

    Created a new script file

---
+++ script3.py
@@ -0,0 +1,31 @@
+
+from tkinter import *
+
+class Window(Frame):
+
+    def __init__(self, master = None):
+        Frame.__init__(self, master)
+
+        self.master = master
+
+        self.init_window()
+
+    def init_window(self):
+        self.master.title("GUI")
+
+        self.pack(fill=BOTH, expand=1)
+
+        #quitButton = Button(self, text="quit", command=self.client_exit)
+        #quitButton.place(x=0, y=0)
+
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
+
+        file = Menu(menu)
+        file.add_command(label="Save", command=self.client_exit)
+        file.add_command(label="Exit", command=self.client_exit)
+        menu.add_cascade(label="File", menu=file)
+
+        edit = Menu(menu)
+        edit.add_command(label="Undo")
+        menu.add_cascade(label="Edit", menu = edit)
